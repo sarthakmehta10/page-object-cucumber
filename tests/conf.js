16af4e45192b700d06d892ef5f0d27ac1fc212d2
Sarthak Mehta <sarthak_mehta@epam.com>
2017-05-12 12:41:58 +0530

    Update conf.js

--- tests/conf.js
+++ tests/conf.js
@@ -2,11 +2,11 @@ exports.config = {
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
     getPageTimeout: 60000,
     allScriptsTimeout: 500000,
-    baseUrl: 'https://www.airbaltic.com/en-ZZ/index',
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
     capabilities: {
-        'browserName': 'chrome'
+        'browserName': process.env.BROWSER,
+        'marionette': true
     },
     specs: [ 'features/*.feature' ],
     cucumberOpts: {
